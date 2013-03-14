075a1a0f34af9884c0edc934e1efd1310ee5c78e
Microfed <microfed@gmail.com>
2013-03-14 10:05:16 +0400

    Implement OAuth 2.0

--- src/common/main.js
+++ src/common/main.js
@@ -1,69 +1,131 @@
 ﻿(function () {
-    "using strict";
+    "use strict";
 
     var YANDEX_READER_URL = 'http://mail.yandex.ru/my/#lenta/group/all',
         YANDEX_STAT_UNREAD_URL = 'https://api-lenta.yandex.ru/stat/unread',
-        YANDEX_AUTH_TOKEN = '487a2d3e0735404fa0c8cd7dd6ccd0c6',
+        EXTENSION_ID = '695982686e1043d8afe881e729503e33',
+        YANDEX_OAUTH_AUTH_SERVICE = 'https://oauth.yandex.ru/authorize?response_type=token&client_id=' + EXTENSION_ID,
+        ICON_16x16 = 'icons/button.png',
+        ICON_16x16_GRAY = 'icons/button_gray.png',
+        TOKEN_NAME = 'token',
+        TURN_OFFLINE_CONFIRM_MESSAGE = "Выключить расширение Yandex Reader Checker?",
 
-        Settings = {
-            refresh_timeout: 10000 // 60 sec
+        settings = {
+            refresh_timeout: 60000, // 60 sec
+            max_count: 999,
+            oauth_token:     kango.storage.getItem(TOKEN_NAME)
         },
+
+        mainLoop,
 
         YandexReaderChecker = function () {
             var self = this;
+
             self.refresh();
+
             kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function () {
                 kango.browser.tabs.create({url: YANDEX_READER_URL});
                 self.refresh();
             });
-            window.setInterval(function () {self.refresh()}, self._refreshTimeout);
+
+            mainLoop = setInterval(function () {self.refresh()}, self._refreshTimeout);
         },
 
         extension;
 
     YandexReaderChecker.prototype = {
 
-        _refreshTimeout: Settings.refresh_timeout,
+        _refreshTimeout: settings.refresh_timeout,
         _feedUrl:        YANDEX_STAT_UNREAD_URL,
 
         _setOffline: function () {
-            kango.ui.browserButton.setIcon('icons/button_gray.png'); // grey icon
+            kango.ui.browserButton.setIcon(ICON_16x16_GRAY); // grey icon
             kango.ui.browserButton.setBadgeValue(0);
         },
 
         _setUnreadCount: function (count) {
-            var text = count > 999 ? 999 + '+' : count;
+            var text = count > settings.max_count ? settings.max_count + '+' : count;
 
             kango.ui.browserButton.setTooltipText(text);
-            kango.ui.browserButton.setIcon('icons/button.png');
+            kango.ui.browserButton.setIcon(ICON_16x16);
             kango.ui.browserButton.setBadgeValue(text);
         },
 
+        _getOAuthToken: function () {
+            var yandexTabID = null,
+                self = this,
+                token,
+                response,
+                checkURL = function (e) {
+                    if (e.target._tab.id === yandexTabID && e.url !== YANDEX_OAUTH_AUTH_SERVICE) {
+                        response = e.url.match(/.*#access_token=(.*)&token_type/);
+
+                        if (response) {
+                            token = response[1];
+                        } else {
+                            if (confirm(TURN_OFFLINE_CONFIRM_MESSAGE)) {
+                                clearInterval(mainLoop);
+                                self._setOffline();
+                            }
+                        }
+
+                        kango.browser.removeEventListener(kango.browser.event.DOCUMENT_COMPLETE, checkURL);
+                        e.target.close();
+
+                        if (token) {
+                            kango.storage.setItem(TOKEN_NAME, token);
+                        }
+                    }
+                };
+
+            kango.browser.tabs.create({url: YANDEX_OAUTH_AUTH_SERVICE});
+
+            kango.browser.tabs.getAll(function (tabs) {
+                tabs.forEach(function (tab) {
+                    if (tab.getUrl() === YANDEX_OAUTH_AUTH_SERVICE) {
+                        yandexTabID = tab._tab.id;
+                    }
+                })
+            });
+
+            kango.browser.addEventListener(kango.browser.event.DOCUMENT_COMPLETE, checkURL);
+        },
+
         refresh: function () {
-            var details = {
+            var details,
+                self = this;
+
+            if (settings.oauth_token) {
+
+                details = {
                     url:         this._feedUrl,
                     async:       true,
                     method:      'GET',
                     contentType: 'application/x-yandex-lenta+xml',
                     headers:     {
-                        authorization: 'OAuth ' + YANDEX_AUTH_TOKEN
+                        authorization: 'OAuth ' + settings.oauth_token
                     }
-                },
-                self = this;
+                };
 
-            kango.xhr.send(details, function (data, xhr) {
-                if (data.status == 200 && data.response != null) {
-                    var count = 0,
-                        matches = data.response.match(/(\d)<\/unread>/);  // Old IE versions doensn't support getElementsByTagNameNS, so we using RegExp
+                kango.xhr.send(details, function (data) {
+                    if (data.status == 200 && data.response != null) {
+                        var count = 0,
+                            matches = data.response.match(/(\d)<\/unread>/);  // Old IE versions doensn't support getElementsByTagNameNS, so we using RegExp
 
-                    if (matches != null && matches.length > 0) {
-                        count = matches[1];
+                        if (matches != null && matches.length > 0) {
+                            count = matches[1];
+                        }
+
+                        self._setUnreadCount(parseInt(count, 10));
+                    } else if (data.status === 401) { // authetication fail
+                        self._getOAuthToken();
+                    } else { // something went wrong
+                        self._setOffline();
                     }
-                    self._setUnreadCount(parseInt(count, 10));
-                } else { // something went wrong
-                    self._setOffline();
-                }
-            });
+                });
+            } else {
+                self._getOAuthToken();
+            }
         }
     };
 
